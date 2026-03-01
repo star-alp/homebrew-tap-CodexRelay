63cfe854e3afd9039bed9d64ba537b6172336073
98yearsoldshark <1839369370@qq.com>
2026-03-01 13:33:24 +0800

    codexrelay 0.1.7

--- Casks/codexrelay.rb
+++ Casks/codexrelay.rb
@@ -1,6 +1,6 @@
 cask "codexrelay" do
-  version "0.1.6"
-  sha256 "74237241012f518e053e0766f4606049503babdefcbc9e8ed7a3910f4abfec1a"
+  version "0.1.7"
+  sha256 "4a98ea119cbb737fcc9fe2c562b21c6d351ed05402d87565f69d6a8ff1c2eec0"
 
   url "https://github.com/Red-noblue/Codex_Relay/releases/download/v#{version}/CodexRelay_#{version}_aarch64.dmg",
       verified: "github.com/Red-noblue/Codex_Relay/"
